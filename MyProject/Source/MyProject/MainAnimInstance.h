5c28519279f4ea491f326057e4101338919ee1b7
pdj2428 <49777470+pdj2428@users.noreply.github.com>
2020-10-18 00:12:10 +0900

    PlayerMove Blueprint
    .

---
+++ MyProject/Source/MyProject/MainAnimInstance.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MainAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API UMainAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	
+	virtual void NativeInitializeAnimation() override;
+
+	UFUNCTION(BlueprintCallable, Category = AnimationProperties)
+	void UpdateAnimationProperties();
+
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = Movemnet)
+	float MovementSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = Movemnet)
+	bool bIsInAir;
+
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = Movemnet)
+	class APawn* Pawn;
+
+
+
+};
